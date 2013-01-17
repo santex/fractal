3cc3985273f98c5432257138ebecb9c7ee16f9f7
nek0 <neko.rloove@rocketmail.com>
2013-01-17 12:33:18 +0100

    colorize carpet

--- carpet.lua
+++ carpet.lua
@@ -3,31 +3,40 @@
 local lgi = require 'lgi'
 local cairo = lgi.cairo
 local math = require 'math'
+require("hsltorgb")
 
 local width = 960
 local height = width
 local surface = cairo.ImageSurface.create('ARGB32', width, height)
 local cr = cairo.Context.create(surface)
 
+local offs = 257
+
 function draw(cr)
-        cr:set_source_rgba(1, 1, 1, 1)
+	local H,S,L = 1,1,0.5
+	local r,g,b = HSLtoRGB(H,S,L)
+        cr:set_source_rgba(r, g, b, 1)
         cr:rectangle(0,0,width,height)
         cr:fill()
-        cr:set_source_rgba(0, 0, 0, 1)
-        carpet(cr,0,0,width,height)
+	H=H+offs
+        carpet(cr,0,0,width,height,H,S,L)
 	surface:write_to_png('carpet.png')
 end
 
-function carpet(cr, sx, sy, dw, dh)
+function carpet(cr, sx, sy, dw, dh, H, S, L)
         if dw > 2 and dh > 2 then
+		local H,S,L = H,S,L
                 local lw,lh = dw/3,dh/3
+		local r,g,b=HSLtoRGB(H,S,L)
+		cr:set_source_rgba(r,g,b,1)
                 cr:rectangle(sx+lw,sy+lh,lw,lh)
                 cr:fill()
+		H=H+offs
                 for k=0,8,1 do
                         if (k ~= 4) then
                                 local i=math.floor(k/3)
                                 local j=k%3
-                                carpet(cr,sx+i*lw,sy+j*lh,lw,lh)
+                                carpet(cr,sx+i*lw,sy+j*lh,lw,lh,H,S,L)
                         end
                 end
         end
---
+++ hsltorgb.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env lua
+
+local math = require 'math'
+
+function HSLtoRGB(H, S, L)
+        local H,S,L = H,S,L
+        while H > 360 do
+                H=H-360
+        end
+        local c = (1-math.abs(2*L-1))*S
+        local h = H/60
+        local x = c*(1-math.abs(h%2-1))
+        local m = L-c*0.5
+        if 0 < h and h < 1 then
+                r1,g1,b1 = c,x,0
+        elseif 1 < h and h < 2 then
+                r1,g1,b1 = x,c,0
+        elseif 2 < h and h < 3 then
+                r1,g1,b1 = 0,c,x
+        elseif 3 < h and h < 4 then
+                r1,g1,b1 = 0,x,c
+        elseif 4 < h and h < 5 then
+                r1,g1,b1 = x,0,c
+        elseif 4 < h and h < 6 then
+                r1,g1,b1 = c,0,x
+        else
+                r1,g1,b1 = 1,0,0
+        end
+        local r,g,b = r1+m,g1+m,b1+m
+        return r,g,b
+end
+
