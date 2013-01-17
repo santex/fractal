78e7b19d7012a451889dc52b53db39e1e024d32e
nek0 <neko.rloove@rocketmail.com>
2013-01-17 12:34:19 +0100

    colorize sierpinski

--- sierpinski.lua
+++ sierpinski.lua
@@ -3,6 +3,7 @@
 local lgi = require 'lgi'
 local cairo = lgi.cairo
 local math = require 'math'
+require("hsltorgb")
 
 local width = 960
 local height = width
@@ -15,7 +16,7 @@ local triargs={
 	{lx=0.25, ly=0},
 }
 
-local offs = 333
+local offs = 257
 
 function draw(cr)
 	local H,S,L=1,1,0.5
@@ -42,7 +43,7 @@ function sierpinski(cr,sx,sy,dw,dh,H,S,L)
 		cr:line_to(sx+0.75*dw,sy+0.5*dh)
 		cr:close_path()
 		local r,g,b = HSLtoRGB(H,S,L)
-		print("r,b,g:",r,g,b)
+		--print("r,b,g:",r,g,b)
 		cr:set_source_rgba(r,g,b,1)
 		cr:fill()
 		H=H+offs
@@ -56,34 +57,5 @@ function sierpinski(cr,sx,sy,dw,dh,H,S,L)
 		end
 	end
 end
-
-function HSLtoRGB(H, S, L)
-	local H,S,L = H,S,L
-	while H > 360 do
-		H=H-360
-	end
-	local c = (1-math.abs(2*L-1))*S
-	local h = H/60
-	local x = c*(1-math.abs(h%2-1))
-	local m = L-c*0.5
-	if 0 < h and h < 1 then
-		r1,g1,b1 = c,x,0
-	elseif 1 < h and h < 2 then
-		r1,g1,b1 = x,c,0
-	elseif 2 < h and h < 3 then
-		r1,g1,b1 = 0,c,x
-	elseif 3 < h and h < 4 then
-		r1,g1,b1 = 0,x,c
-	elseif 4 < h and h < 5 then
-		r1,g1,b1 = x,0,c
-	elseif 4 < h and h < 6 then
-		r1,g1,b1 = c,0,x
-	else
-		r1,g1,b1 = 1,0,0
-	end
-	local r,g,b = r1+m,g1+m,b1+m
-	return r,g,b
-end
-
 
 draw(cr)
